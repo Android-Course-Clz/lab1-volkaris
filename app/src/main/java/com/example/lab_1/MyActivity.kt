427f43ce8880dbab579e729d38698cdc2a12f404
Ilya <zaxsad109@gmail.com>
2025-02-28 17:34:51 +0300

    /

---
+++ app/src/main/java/com/example/lab_1/MyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lab_1
+
+import android.app.Activity
+import android.os.Bundle
+
+class MyActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.layout)
+    }
+}
